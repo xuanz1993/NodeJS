2e4a5ff5c361be0548e5b8156ba78bbd762b1a92
Xuan Zhang <xuan@converzion.co>
2017-08-01 17:04:35 +0800

    initial commit

---
+++ 1-node-intro-m1-intro-exercise-files/server.js
@@ -0,0 +1,17 @@
+// process.env.PORT and process.env.IP are for Cloud9
+// substitute with your own port and (optionally) IP as required
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
